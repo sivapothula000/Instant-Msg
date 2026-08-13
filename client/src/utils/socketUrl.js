13b5653dcfc84b5712f8281e9c5171a3662a088f
Pothula Siva Anand <sivaanandpothula1234@gmail.com>
2026-08-13 09:38:44 +0530

    Refactor getSocketUrl to prioritize VITE_SERVER_URL

--- client/src/utils/socketUrl.js
+++ client/src/utils/socketUrl.js
@@ -1,15 +1,11 @@
 export const getSocketUrl = () => {
-  if (import.meta.env.VITE_SERVER_URL) {
-    return import.meta.env.VITE_SERVER_URL;
-  }
-
   if (import.meta.env.VITE_SOCKET_URL) {
     return import.meta.env.VITE_SOCKET_URL;
   }
 
-  if (import.meta.env.PROD) {
-    return "https://instant-msg-s0q9.onrender.com/";
+  if (import.meta.env.VITE_SERVER_URL) {
+    return import.meta.env.VITE_SERVER_URL;
   }
 
-  return "https://instant-msg-s0q9.onrender.com/";
+  return "https://instant-msg-s0q9.onrender.com";
 };
